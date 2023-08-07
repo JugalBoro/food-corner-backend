b0d65a4d3de9982acbce698738b3514b60bf1455
Jugal <jboro999@gmail.com>
2023-08-07 15:45:09 +0530

    Update server.js
    added frontend uri

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const dotanv = require("dotenv");
 const { bgCyan } = require("colors");
 require("colors");
+const corsOptions = {
+    origin: "https://food-corner-frontend.onrender.com/", // frontend URI (ReactJS)
+}
+
 const connectDb = require("./config/config");
 //dotenv config
 dotanv.config();
@@ -17,6 +21,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
