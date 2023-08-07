9ebf6deaec3ca4001dc9de2ae27556d5e596d87b
Jugal <jboro999@gmail.com>
2023-08-08 01:32:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 app.use(cors(corsOptions));
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.configure(function() {
+    app.use(allowCrossDomain);
+    //some other code
+});  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
